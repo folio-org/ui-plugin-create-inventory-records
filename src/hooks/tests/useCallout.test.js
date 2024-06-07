9ac3c105c52add1c25f04199943e3e590e586377
Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
2024-06-07 14:28:55 +0300

    UIPCIR-71: Jest/RTL: Cover `useCallout` hook with unit tests (#175)

---
+++ src/hooks/tests/useCallout.test.js
@@ -0,0 +1,18 @@
+import { renderHook } from '@folio/jest-config-stripes/testing-library/react';
+
+import '../../../test/jest/__mock__';
+
+import { CalloutContext } from '@folio/stripes/core';
+import useCallout from '../useCallout';
+
+const wrapper = ({ children }) => (
+  <CalloutContext.Provider value={{ sendCallout: new Function() }}>{children}</CalloutContext.Provider>
+);
+
+describe('useCallout hook', () => {
+  it('should return callout', () => {
+    const { result } = renderHook(() => useCallout(), { wrapper });
+
+    expect(result.current).toMatchObject({ sendCallout: expect.any(Function) });
+  });
+});
